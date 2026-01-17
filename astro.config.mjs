6dbcef0dac77dfdc77220436db3000958fe0d9dd
Lily Williams <86631567+LFW25@users.noreply.github.com>
2026-01-16 20:43:16 -0500

    Fix: Export to custom URL\

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
-
-export default {
-  site: "https://lilyfrancesca.cv"
-};
+export default defineConfig({
+  site: "https://lilyfrancesca.cv",
+});
